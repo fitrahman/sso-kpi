4138b39efb8fc5a985cd445fe47de85bdef89285
fitrahman <fitrahmanihsan@gmail.com>
2026-08-04 13:55:57 +0700

    feat: Add PegawaiSSOUserSeeder to seed dummy pegawai users on SSO server

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // 1. Run User Seeder
         $this->call(AdminUserSeeder::class);
+        $this->call(PegawaiSSOUserSeeder::class);
 
         $users = User::all();
     }
